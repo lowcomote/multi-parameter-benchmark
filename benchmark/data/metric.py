f0791d832f6247ccdecd805107c1bb6da2564fab
Benedek Horvath <benedek.horvath@incquerylabs.com>
2022-04-18 19:30:20 +0200

    Fix compile errors in metric.py.

--- benchmark/data/metric.py
+++ benchmark/data/metric.py
@@ -1,52 +1,69 @@
+# to type hint a method with the type of the enclosing class: https://stackoverflow.com/a/33533514
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from pickletools import long1
+
 
 @dataclass
 class Metric(ABC):
-    
-    @abstractmethod
-    def gt(v: Metric) -> bool: pass
 
     @abstractmethod
-    def gteq(v: Metric) -> bool: pass
+    def gt(self, other: Metric) -> bool:
+        pass
 
     @abstractmethod
-    def lt(v: Metric) -> bool: pass
-    
+    def gteq(self, other: Metric) -> bool:
+        pass
+
     @abstractmethod
-    def lteq(v: Metric) -> bool: pass
+    def lt(self, other: Metric) -> bool:
+        pass
+
+    @abstractmethod
+    def lteq(self, other: Metric) -> bool:
+        pass
+
 
 @dataclass
-class LongMetric(Metric): 
+class LongMetric(Metric):
 
-    def __init__(self, value: long) -> None:
+    def __init__(self, value: int) -> None:
         super().__init__()
-        self._value = value    
-    
-    def gt(v: Metric) -> bool: True # TODO
+        self._value = value
 
-    def gteq(v: Metric) -> bool: True # TODO
+    def gt(self, other: Metric) -> bool:
+        return True  # TODO
 
-    def lt(v: Metric) -> bool: False # TODO
-    
-    def lteq(v: Metric) -> bool: False # TODO
+    def gteq(self, other: Metric) -> bool:
+        return True  # TODO
+
+    def lt(self, other: Metric) -> bool:
+        return False  # TODO
+
+    def lteq(self, other: Metric) -> bool:
+        return False  # TODO
+
 
 @dataclass
 class Tuple2Metric(Metric):
 
     def __init__(self, v1: Metric, v2: Metric) -> None:
         super().__init__()
-        self._v1 = v1    
-        self._v2 = v2    
-    
-    def gt(v: Metric) -> bool: True # TODO
+        self._v1 = v1
+        self._v2 = v2
 
-    def gteq(v: Metric) -> bool: True # TODO
+    def gt(self, other: Metric) -> bool:
+        return True  # TODO
 
-    def lt(v: Metric) -> bool: False # TODO
-    
-    def lteq(v: Metric) -> bool: False # TODO
+    def gteq(self, other: Metric) -> bool:
+        return True  # TODO
+
+    def lt(self, other: Metric) -> bool:
+        return False  # TODO
+
+    def lteq(self, other: Metric) -> bool:
+        return False  # TODO
 
 
 @dataclass
@@ -54,14 +71,18 @@ class Tuple3Metric(Metric):
 
     def __init__(self, v1: Metric, v2: Metric, v3: Metric) -> None:
         super().__init__()
-        self._v1 = v1    
-        self._v2 = v2    
-        self._v3 = v3    
-    
-    def gt(v: Metric) -> bool: True # TODO
+        self._v1 = v1
+        self._v2 = v2
+        self._v3 = v3
 
-    def gteq(v: Metric) -> bool: True # TODO
+    def gt(self, other: Metric) -> bool:
+        return True  # TODO
 
-    def lt(v: Metric) -> bool: False # TODO
-    
-    def lteq(v: Metric) -> bool: False # TODO
+    def gteq(self, other: Metric) -> bool:
+        return True  # TODO
+
+    def lt(self, other: Metric) -> bool:
+        return False  # TODO
+
+    def lteq(self, other: Metric) -> bool:
+        return False  # TODO
