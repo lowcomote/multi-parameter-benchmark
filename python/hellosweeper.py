13cf45efcae7c22bc4243aa0a8cf031319ccb5b9
jolan <jolan.philippe@gmail.com>
2022-05-23 11:54:18 +0200

    Finished hellosweeper example

--- python/hellosweeper.py
+++ python/hellosweeper.py
@@ -1,25 +1,29 @@
 from benchmark.sweeper.sweep import Sweeper
 from benchmark.data.config import ApplicationParameter
 from benchmark.data.metric import LongMetric
+from random import randrange
 
 def bench(config):
-    value = int(config['prime']) + int(config['odd']) + int(config['even']) + int(config['fibo'])
+    value = int(config['prime']) + int(config['odd']) + int(config['even']) + int(config['fibo']) + randrange(5)
     print("test of", config,":", value)
     return LongMetric(value)
 
+
+test = 10
 
 prime = ApplicationParameter("prime", 1, ["1", "2", "3", "5", "7", "11"], None)
 odd = ApplicationParameter("odd", 2, ["1", "3", "5", "7", "9", "11"], None)
 even = ApplicationParameter("even", 3, ["2", "4", "6", "8", "10", "12"], None)
 fibo = ApplicationParameter("fibo", 4, ["1", "2", "3", "5", "8", "13", "21"], None)
 
-sweeper = Sweeper([prime, odd, even, fibo], 4, remove_workdir=True)
+sweeper = Sweeper([prime, odd, even, fibo], 5, remove_workdir=True)
 
 print(sweeper)
 
 config = sweeper.get_next()
 while config != None:
-    sweeper.score(config, bench(config))
+    for _ in range(test):
+        sweeper.score(config, bench(config))
     # print(f"current best {sweeper.best()}")
     sweeper.done(config)
     config = sweeper.get_next()
